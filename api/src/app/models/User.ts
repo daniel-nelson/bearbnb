a87f6df63aaefbcef4ab5dda7fdc6edfaaa4b8af
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-03-10 08:34:45 -0500

    Generate User model
    The User model is primarily used for authentication
    and associating the authenticated user with resources.
    Since we haven't selected an authentication strategy
    yet, we'll simply start with an email address and phone
    number.
    
    `citext` ensures both that when we create a unique
    constraint, the email address will be case-insensitively
    unique, and that all queries are automatically case-
    insensitve.
    
    `phone:encrypted` will result in a database field of `encryptedPhone`
    and a phone property on the model. The data is automatically
    encrypted upon setting and decrypted upon getting.
    
    ```console
    cd api
    pnpm psy db:create
    pnpm psy g:model --no-serializer User email:citext phone:encrypted
    ```

---
+++ api/src/app/models/User.ts
@@ -0,0 +1,19 @@
+import { Decorators } from '@rvoh/dream'
+import { DreamColumn } from '@rvoh/dream/types'
+import ApplicationModel from '@models/ApplicationModel.js'
+
+const deco = new Decorators<typeof User>()
+
+export default class User extends ApplicationModel {
+  public override get table() {
+    return 'users' as const
+  }
+
+  public id: DreamColumn<User, 'id'>
+  public email: DreamColumn<User, 'email'>
+  public createdAt: DreamColumn<User, 'createdAt'>
+  public updatedAt: DreamColumn<User, 'updatedAt'>
+
+  @deco.Encrypted()
+  public phone: DreamColumn<User, 'encryptedPhone'>
+}
