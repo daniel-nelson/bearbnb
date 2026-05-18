0671a742faea08dd2c8c7973e9e2d55450d92241
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-05-18 15:50:47 -0500

    Set up polymorphic associations between LocalizedText and Host/Place/Room
    Create a default LocalizedText for each Host/Place/Room
    
    ```console
    pnpm psy db:migrate
    pnpm uspec spec/unit/models
    ```
    
    Also flesh out the LocalizedText controller. Since LocalizedText belongs to different types (Host/Place/Room), restricting the controller to only allow access to owned LocalizedText requires a different strategy. The switch statement used is future proofed so that if LocalizedText is added to another model in the future, access will automatically be restricted until it is explicitly added to the controller. Note that not-found (404) is a design choice that aligns with the not-found response returned when associationQuery(...).findOrFail(...) is used to limit access to owned resources; however, forbidden (403) could have been used instead.
    
    ```console
    pnpm uspec spec/unit/controllers/V1/Host/LocalizedTextsController.spec.ts
    ```
    
    Before committing, ensure all specs pass:
    
    ```console
    pnpm uspec
    ```

--- api/spec/unit/models/Room.spec.ts
+++ api/spec/unit/models/Room.spec.ts
@@ -1,3 +1,21 @@
+import createLocalizedText from '@spec/factories/LocalizedTextFactory.js'
+import createDen from '@spec/factories/Room/DenFactory.js'
+
 describe('Room', () => {
-  it.todo('add a test here to get started building Room')
+  it('has many LocalizedTexts', async () => {
+    const room = await createDen()
+    const esLocalizedText = await createLocalizedText({ localizable: room, locale: 'es-ES' })
+
+    const localizedText = await room.associationQuery('localizedTexts', { and: { locale: 'es-ES' } }).last()
+    expect(localizedText).toMatchDreamModel(esLocalizedText)
+  })
+
+  context('upon creation', () => {
+    it('creates en-US LocalizedText for the Room', async () => {
+      const room = await createDen()
+      const localizedText = await room.associationQuery('localizedTexts').firstOrFail()
+
+      expect(localizedText.locale).toEqual('en-US')
+    })
+  })
 })
--- api/src/app/controllers/V1/Host/LocalizedTextsController.ts
+++ api/src/app/controllers/V1/Host/LocalizedTextsController.ts
@@ -1,7 +1,9 @@
 import { OpenAPI } from '@rvoh/psychic'
 import { DreamParamSafeColumnNames } from '@rvoh/dream/types'
+import Room from '@models/Room.js'
 import V1HostBaseController from './BaseController.js'
 import LocalizedText from '@models/LocalizedText.js'
+import { type LocalizedTypesEnum } from '@src/types/db.js'
 
 const openApiTags = ['localized-texts']
 
@@ -18,9 +20,9 @@ export default class V1HostLocalizedTextsController extends V1HostBaseController
     },
   })
   public async update() {
-    // const localizedText = await this.localizedText()
-    // await localizedText.update(this.extractParams(LocalizedText, paramSafeColumns))
-    // this.noContent()
+    const localizedText = await this.localizedText()
+    await localizedText.update(this.extractParams(LocalizedText, paramSafeColumns))
+    this.noContent()
   }
 
   @OpenAPI({
@@ -30,14 +32,34 @@ export default class V1HostLocalizedTextsController extends V1HostBaseController
     fastJsonStringify: true,
   })
   public async destroy() {
-    // const localizedText = await this.localizedText()
-    // await localizedText.destroy()
-    // this.noContent()
+    const localizedText = await this.localizedText()
+    await localizedText.destroy()
+    this.noContent()
   }
 
   private async localizedText() {
-    // return await this.currentUser.associationQuery('localizedTexts')
-    //   .preloadFor('default')
-    //   .findOrFail(this.castParam('id', 'string'))
+    const id = this.castParam('id', 'uuid')
+    const localizedText = await LocalizedText.findOrFail(id)
+    const localizableType: LocalizedTypesEnum = localizedText.localizableType
+
+    switch (localizableType) {
+      case 'Host':
+        return await this.currentHost.associationQuery('localizedTexts').preloadFor('default').findOrFail(id)
+      case 'Place': {
+        const place = await this.currentHost
+          .associationQuery('places')
+          .findOrFail(localizedText.localizableId)
+        return await place.associationQuery('localizedTexts').preloadFor('default').findOrFail(id)
+      }
+      case 'Room': {
+        const room = await Room.findOrFail(localizedText.localizableId)
+        await this.currentHost.associationQuery('places').findOrFail(room.placeId)
+        return await room.associationQuery('localizedTexts').preloadFor('default').findOrFail(id)
+      }
+      default: {
+        const _never: never = localizableType
+        throw new Error(`Unhandled LocalizedTypesEnum: ${String(_never)}`)
+      }
+    }
   }
 }
