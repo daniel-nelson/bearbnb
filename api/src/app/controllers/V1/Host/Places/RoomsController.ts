55dda6bf07c9785b1d8f6bf482110340184aceb1
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-05-18 15:44:23 -0500

    Rooms controller specs passing
    NOTE: if, after running `pnpm psy sync`, your editor starts showing errors like
    `Unsafe argument of type error typed assigned to a parameter of type 'undefined'.`
    OR
    `Unsafe assignment of an error typed value.`,
    restart the ESLint server (the `ESLint: Restart ESLint Server`
    command in VSCode / Cursor)
    
    Note the changes in api/spec/unit/controllers/V1/Host/Places/RoomsController.spec.ts:
    
    1. The room is no longer the base STI model because, in STI, we cannot instantiate the base model, only child models, so we choose an arbitrary Room child to stand in for any Room.
    2. The request body to the create and update endpoints is changed to reflect the STI child we are creating/updating.
    
    ```console
    pnpm psy sync
    pnpm build:spec # to surface any errors so you can fix them
    pnpm uspec spec/unit/controllers/V1/Host/Places/RoomsController.spec.ts
    ```

--- api/src/app/controllers/V1/Host/Places/RoomsController.ts
+++ api/src/app/controllers/V1/Host/Places/RoomsController.ts
@@ -1,11 +1,22 @@
 import { OpenAPI } from '@rvoh/psychic'
 import { DreamParamSafeColumnNames } from '@rvoh/dream/types'
+import Bathroom from '@models/Room/Bathroom.js'
+import Bedroom from '@models/Room/Bedroom.js'
+import Den from '@models/Room/Den.js'
+import Kitchen from '@models/Room/Kitchen.js'
+import LivingRoom from '@models/Room/LivingRoom.js'
+import { RoomTypesEnumValues } from '@src/types/db.js'
 import V1HostPlacesBaseController from './BaseController.js'
 import Room from '@models/Room.js'
 
 const openApiTags = ['rooms']
 
-const paramSafeColumns: DreamParamSafeColumnNames<Room>[] = ['position']
+const paramSafeColumns: DreamParamSafeColumnNames<Room>[] = [
+  'appliances',
+  'bathOrShowerStyle',
+  'bedTypes',
+  'position',
+]
 
 export default class V1HostPlacesRoomsController extends V1HostPlacesBaseController {
   @OpenAPI(Room, {
@@ -17,10 +28,11 @@ export default class V1HostPlacesRoomsController extends V1HostPlacesBaseControl
     fastJsonStringify: true,
   })
   public async index() {
-    // const rooms = await this.currentPlace.associationQuery('rooms')
-    //   .preloadFor('summary')
-    //   .cursorPaginate({ cursor: this.castParam('cursor', 'string', { allowNull: true }) })
-    // this.ok(rooms)
+    const rooms = await this.currentPlace
+      .associationQuery('rooms')
+      .preloadFor('summary')
+      .cursorPaginate({ cursor: this.castParam('cursor', 'string', { allowNull: true }) })
+    this.ok(rooms)
   }
 
   @OpenAPI(Room, {
@@ -30,8 +42,8 @@ export default class V1HostPlacesRoomsController extends V1HostPlacesBaseControl
     fastJsonStringify: true,
   })
   public async show() {
-    // const room = await this.room()
-    // this.ok(room)
+    const room = await this.room()
+    this.ok(room)
   }
 
   @OpenAPI(Room, {
@@ -41,12 +53,38 @@ export default class V1HostPlacesRoomsController extends V1HostPlacesBaseControl
     fastJsonStringify: true,
     requestBody: {
       only: paramSafeColumns,
+      including: ['type'],
     },
   })
   public async create() {
-    // let room = await this.currentPlace.createAssociation('rooms', this.extractParams(Room, paramSafeColumns))
-    // if (room.isPersisted) room = await room.loadFor('default').execute()
-    // this.created(room)
+    let room: Room
+    const roomType = this.castParam('type', 'string', { enum: RoomTypesEnumValues })
+    const roomParams = this.extractParams(Room, paramSafeColumns)
+
+    switch (roomType) {
+      case 'Bathroom':
+        room = await Bathroom.create({ place: this.currentPlace, ...roomParams })
+        break
+      case 'Bedroom':
+        room = await Bedroom.create({ place: this.currentPlace, ...roomParams })
+        break
+      case 'Den':
+        room = await Den.create({ place: this.currentPlace, ...roomParams })
+        break
+      case 'Kitchen':
+        room = await Kitchen.create({ place: this.currentPlace, ...roomParams })
+        break
+      case 'LivingRoom':
+        room = await LivingRoom.create({ place: this.currentPlace, ...roomParams })
+        break
+      default: {
+        const _never: never = roomType
+        throw new Error(`Unhandled RoomTypesEnum: ${String(_never)}`)
+      }
+    }
+
+    if (room.isPersisted) room = await room.loadFor('default').execute()
+    this.created(room)
   }
 
   @OpenAPI(Room, {
@@ -59,9 +97,9 @@ export default class V1HostPlacesRoomsController extends V1HostPlacesBaseControl
     },
   })
   public async update() {
-    // const room = await this.room()
-    // await room.update(this.extractParams(Room, paramSafeColumns))
-    // this.noContent()
+    const room = await this.room()
+    await room.update(this.extractParams(Room, paramSafeColumns))
+    this.noContent()
   }
 
   @OpenAPI({
@@ -71,14 +109,15 @@ export default class V1HostPlacesRoomsController extends V1HostPlacesBaseControl
     fastJsonStringify: true,
   })
   public async destroy() {
-    // const room = await this.room()
-    // await room.destroy()
-    // this.noContent()
+    const room = await this.room()
+    await room.destroy()
+    this.noContent()
   }
 
   private async room() {
-    // return await this.currentPlace.associationQuery('rooms')
-    //   .preloadFor('default')
-    //   .findOrFail(this.castParam('id', 'string'))
+    return await this.currentPlace
+      .associationQuery('rooms')
+      .preloadFor('default')
+      .findOrFail(this.castParam('id', 'uuid'))
   }
 }
