2d73645d708ea63e9f4d89e3aaacf8b8c026cb95
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-05-18 15:37:48 -0500

    Places index endpoint includes the name
    ```console
    pnpm psy sync --help
    pnpm psy sync # to update the OpenAPI spec after updating the serializer
    pnpm uspec spec/unit/controllers/V1/Host/PlacesController.spec.ts
    ```

--- api/src/types/openapi/tests.openapi.d.ts
+++ api/src/types/openapi/tests.openapi.d.ts
@@ -216,6 +216,7 @@ export interface components {
         };
         PlaceSummary: {
             id: string;
+            name: string;
         };
         ValidationErrors: {
             /** @enum {string} */
