e1e45254a8f04a26ed7497ab72254fd9b4020c37
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-05-18 15:33:27 -0500

    Generate Place resource
    ```console
    pnpm psy g:resource --help
    pnpm psy g:resource --owning-model=Host v1/host/places Place name:citext style:enum:place_styles:cottage,cabin,lean_to,treehouse,tent,cave,dump sleeps:integer
    ```

---
+++ api/src/app/controllers/V1/Host/PlacesController.ts
@@ -0,0 +1,84 @@
+import { OpenAPI } from '@rvoh/psychic'
+import { DreamParamSafeColumnNames } from '@rvoh/dream/types'
+import V1HostBaseController from './BaseController.js'
+import Place from '@models/Place.js'
+
+const openApiTags = ['places']
+
+const paramSafeColumns: DreamParamSafeColumnNames<Place>[] = ['name', 'style', 'sleeps']
+
+export default class V1HostPlacesController extends V1HostBaseController {
+  @OpenAPI(Place, {
+    status: 200,
+    tags: openApiTags,
+    description: 'Paginated index of Places',
+    cursorPaginate: true,
+    serializerKey: 'summary',
+    fastJsonStringify: true,
+  })
+  public async index() {
+    // const places = await this.currentHost.associationQuery('places')
+    //   .preloadFor('summary')
+    //   .cursorPaginate({ cursor: this.castParam('cursor', 'string', { allowNull: true }) })
+    // this.ok(places)
+  }
+
+  @OpenAPI(Place, {
+    status: 200,
+    tags: openApiTags,
+    description: 'Fetch a Place',
+    fastJsonStringify: true,
+  })
+  public async show() {
+    // const place = await this.place()
+    // this.ok(place)
+  }
+
+  @OpenAPI(Place, {
+    status: 201,
+    tags: openApiTags,
+    description: 'Create a Place',
+    fastJsonStringify: true,
+    requestBody: {
+      only: paramSafeColumns,
+    },
+  })
+  public async create() {
+    // let place = await this.currentHost.createAssociation('places', this.extractParams(Place, paramSafeColumns))
+    // if (place.isPersisted) place = await place.loadFor('default').execute()
+    // this.created(place)
+  }
+
+  @OpenAPI(Place, {
+    status: 204,
+    tags: openApiTags,
+    description: 'Update a Place',
+    fastJsonStringify: true,
+    requestBody: {
+      only: paramSafeColumns,
+    },
+  })
+  public async update() {
+    // const place = await this.place()
+    // await place.update(this.extractParams(Place, paramSafeColumns))
+    // this.noContent()
+  }
+
+  @OpenAPI({
+    status: 204,
+    tags: openApiTags,
+    description: 'Destroy a Place',
+    fastJsonStringify: true,
+  })
+  public async destroy() {
+    // const place = await this.place()
+    // await place.destroy()
+    // this.noContent()
+  }
+
+  private async place() {
+    // return await this.currentHost.associationQuery('places')
+    //   .preloadFor('default')
+    //   .findOrFail(this.castParam('id', 'string'))
+  }
+}
