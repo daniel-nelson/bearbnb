3f0fbcadc281f9416df239266cc3931f6b374469
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-05-18 15:41:14 -0500

    Generate Room/Kitchen STI model
    ```console
    pnpm psy g:sti-child --help
    pnpm psy g:sti-child --model-name=Kitchen Room/Kitchen extends Room 'appliances:enum[]:appliance_types:stove,oven,microwave,dishwasher'
    pnpm psy db:migrate
    ```

--- api/src/types/dream.ts
+++ api/src/types/dream.ts
@@ -63,10 +63,12 @@ import {
   type ClockTimeTz,
 } from '@rvoh/dream'
 import {
+  type ApplianceTypesEnum,
   type BathOrShowerStylesEnum,
   type BedTypesEnum,
   type PlaceStylesEnum,
   type RoomTypesEnum,
+  ApplianceTypesEnumValues,
   BathOrShowerStylesEnumValues,
   BedTypesEnumValues,
   PlaceStylesEnumValues,
@@ -427,6 +429,7 @@ export const schema = {
       named: [],
     },
     nonJsonColumnNames: [
+      'appliances',
       'bathOrShowerStyle',
       'bedTypes',
       'createdAt',
@@ -438,6 +441,15 @@ export const schema = {
       'updatedAt',
     ],
     columns: {
+      appliances: {
+        coercedType: {} as ApplianceTypesEnum[],
+        enumType: {} as ApplianceTypesEnum,
+        enumArrayType: [] as ApplianceTypesEnum[],
+        enumValues: ApplianceTypesEnumValues,
+        dbType: 'appliance_types_enum[]',
+        allowNull: false,
+        isArray: true,
+      },
       bathOrShowerStyle: {
         coercedType: {} as BathOrShowerStylesEnum | null,
         enumType: {} as BathOrShowerStylesEnum,
@@ -639,6 +651,7 @@ export const connectionTypeConfig = {
       Room: 'rooms',
       'Room/Bathroom': 'rooms',
       'Room/Bedroom': 'rooms',
+      'Room/Kitchen': 'rooms',
       User: 'users',
     },
   },
