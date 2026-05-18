f46f2ebb97e165295524020bc8dada941156cf35
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-05-18 15:52:23 -0500

    Leverage a passthrough and condition
    to create a HasOne currentLocalizedText
    association for displaying localized
    text to Guests
    
    ```console
    pnpm psy sync
    pnpm uspec spec/unit/models/Host.spec.ts
    pnpm uspec spec/unit/models/Place.spec.ts
    pnpm uspec spec/unit/models/Room.spec.ts
    pnpm uspec
    ```

--- api/src/app/models/Host.ts
+++ api/src/app/models/Host.ts
@@ -1,4 +1,4 @@
-import { Decorators, SoftDelete } from '@rvoh/dream'
+import { Decorators, DreamConst, SoftDelete } from '@rvoh/dream'
 import { DreamColumn, DreamSerializers } from '@rvoh/dream/types'
 import ApplicationModel from '@models/ApplicationModel.js'
 import HostPlace from '@models/HostPlace.js'
@@ -43,4 +43,11 @@ export default class Host extends ApplicationModel {
   public async createDefaultLocalizedText(this: Host) {
     await this.createAssociation('localizedTexts', { locale: 'en-US' })
   }
+
+  @deco.HasOne('LocalizedText', {
+    polymorphic: true,
+    on: 'localizableId',
+    and: { locale: DreamConst.passthrough },
+  })
+  public currentLocalizedText: LocalizedText
 }
