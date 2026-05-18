2692c01dbb9423246affd6d1a00ef0c2d9bd2895
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-05-18 15:41:14 -0500

    Generate Room/Kitchen STI model
    ```console
    pnpm psy g:sti-child --help
    pnpm psy g:sti-child --model-name=Kitchen Room/Kitchen extends Room 'appliances:enum[]:appliance_types:stove,oven,microwave,dishwasher'
    pnpm psy db:migrate
    ```

---
+++ api/src/app/models/Room/Kitchen.ts
@@ -0,0 +1,19 @@
+import { STI } from '@rvoh/dream'
+import { DreamColumn, DreamSerializers } from '@rvoh/dream/types'
+import Room from '@models/Room.js'
+
+// Uncomment when adding decorators (@deco.BelongsTo, @deco.Validates, etc.):
+// import { Decorators } from '@rvoh/dream'
+// const deco = new Decorators<typeof Kitchen>()
+
+@STI(Room)
+export default class Kitchen extends Room {
+  public override get serializers(): DreamSerializers<Kitchen> {
+    return {
+      default: 'Room/KitchenSerializer',
+      summary: 'Room/KitchenSummarySerializer',
+    }
+  }
+
+  public appliances: DreamColumn<Kitchen, 'appliances'>
+}
