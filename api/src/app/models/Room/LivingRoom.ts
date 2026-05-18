ff7651e4552f5a0cb70e4d000a99c3f8379acf58
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-05-18 15:42:10 -0500

    Generate Room/Den and Room/LivingRoom STI models
    ```console
    pnpm psy g:sti-child --help
    pnpm psy g:sti-child --model-name=Den Room/Den extends Room
    pnpm psy g:sti-child --model-name=LivingRoom Room/LivingRoom extends Room
    pnpm psy sync
    ```

---
+++ api/src/app/models/Room/LivingRoom.ts
@@ -0,0 +1,18 @@
+import { STI } from '@rvoh/dream'
+import { DreamColumn, DreamSerializers } from '@rvoh/dream/types'
+import Room from '@models/Room.js'
+
+// Uncomment when adding decorators (@deco.BelongsTo, @deco.Validates, etc.):
+// import { Decorators } from '@rvoh/dream'
+// const deco = new Decorators<typeof LivingRoom>()
+
+@STI(Room)
+export default class LivingRoom extends Room {
+  public override get serializers(): DreamSerializers<LivingRoom> {
+    return {
+      default: 'Room/LivingRoomSerializer',
+      summary: 'Room/LivingRoomSummarySerializer',
+    }
+  }
+
+}
