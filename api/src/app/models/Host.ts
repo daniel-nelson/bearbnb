3059d3e6b0c3e98f197577de5862a13a1fc3a70e
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-06-30 11:02:06 -0500

    Generate Host model
    ```console
    pnpm psy g:model --help
    pnpm psy g:model Host User:belongs_to
    ```

---
+++ api/src/app/models/Host.ts
@@ -0,0 +1,29 @@
+import { Decorators, SoftDelete } from '@rvoh/dream'
+import { DreamColumn, DreamSerializers } from '@rvoh/dream/types'
+import ApplicationModel from '@models/ApplicationModel.js'
+import User from '@models/User.js'
+
+const deco = new Decorators<typeof Host>()
+
+@SoftDelete()
+export default class Host extends ApplicationModel {
+  public override get table() {
+    return 'hosts' as const
+  }
+
+  public get serializers(): DreamSerializers<Host> {
+    return {
+      default: 'HostSerializer',
+      summary: 'HostSummarySerializer',
+    }
+  }
+
+  public id: DreamColumn<Host, 'id'>
+  public createdAt: DreamColumn<Host, 'createdAt'>
+  public updatedAt: DreamColumn<Host, 'updatedAt'>
+  public deletedAt: DreamColumn<Host, 'deletedAt'>
+
+  @deco.BelongsTo('User', { on: 'userId' })
+  public user: User
+  public userId: DreamColumn<Host, 'userId'>
+}
