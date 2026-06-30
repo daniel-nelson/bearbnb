1a38bb8803549c3c8beed1e11c3c6a795ddbca19
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-06-30 11:14:30 -0500

    Generate Room/Bedroom STI model
    ```console
    pnpm psy g:sti-child --help
    pnpm psy g:sti-child --model-name=Bedroom Room/Bedroom extends Room 'bed_types:enum[]:bed_types:twin,bunk,queen,king,cot,sofabed'
    pnpm psy db:migrate
    ```

---
+++ api/src/app/models/Room/Bedroom.ts
@@ -0,0 +1,15 @@
+import { STI } from '@rvoh/dream'
+import { DreamColumn, DreamSerializers } from '@rvoh/dream/types'
+import Room from '@models/Room.js'
+
+@STI(Room)
+export default class Bedroom extends Room {
+  public override get serializers(): DreamSerializers<Bedroom> {
+    return {
+      default: 'Room/BedroomSerializer',
+      summary: 'Room/BedroomSummarySerializer',
+    }
+  }
+
+  public bedTypes: DreamColumn<Bedroom, 'bedTypes'>
+}
