d15ee9ea2c457ca2ab28ab3ed84858bd5730cc48
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-06-30 11:18:43 -0500

    Generate LocalizedText resource
    ```console
    pnpm psy g:resource --help
    pnpm psy g:resource --only=update,destroy v1/host/localized-texts LocalizedText localizable_type:enum:localized_types:Host,Place,Room localizable_id:uuid locale:enum:locales:en-US,es-ES title:string markdown:text
    ```
    
    Leverages `--only=update,destroy` to only generate the update and destroy actions. Each polymorphically associated model's controller will have an action for creating an associated LocalizedText model, and Host-facing serializers can display LocalizedTexts, but the actions for maintaining all LocalizedText will be centralized in the LocalizedText controller.

---
+++ api/src/app/serializers/LocalizedTextSerializer.ts
@@ -0,0 +1,14 @@
+import { DreamSerializer } from '@rvoh/dream'
+import LocalizedText from '@models/LocalizedText.js'
+
+export const LocalizedTextSummarySerializer = (localizedText: LocalizedText) =>
+  DreamSerializer(LocalizedText, localizedText)
+    .attribute('id')
+
+export const LocalizedTextSerializer = (localizedText: LocalizedText) =>
+  LocalizedTextSummarySerializer(localizedText)
+    .attribute('localizableType')
+    .attribute('localizableId')
+    .attribute('locale')
+    .attribute('title')
+    .attribute('markdown')
