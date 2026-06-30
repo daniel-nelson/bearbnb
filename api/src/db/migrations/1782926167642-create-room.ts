2d67043a28a500dbdd25ed41a6eb6643d7526280
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-06-30 11:12:56 -0500

    Generate Room resource
    Command run:
    
    pnpm psy g:resource --help
    pnpm psy g:resource --sti-base-serializer --owning-model=Place 'v1/host/places/{}/rooms' Room type:enum:room_types:Bathroom,Bedroom,Kitchen,Den,LivingRoom Place:belongs_to position:integer:optional
    
    The places/{} path makes rooms a nested resource within the places resource.
    
    pnpm psy db:migrate
    pnpm psy routes

---
+++ api/src/db/migrations/1782926167642-create-room.ts
@@ -0,0 +1,55 @@
+import { Kysely, sql } from 'kysely'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createType('room_types_enum')
+    .asEnum([
+      'Bathroom',
+      'Bedroom',
+      'Kitchen',
+      'Den',
+      'LivingRoom'
+    ])
+    .execute()
+
+  await db.schema
+    .createTable('rooms')
+    .addColumn('id', 'uuid', col =>
+      col
+        .primaryKey()
+        .defaultTo(sql`uuidv7()`),
+    )
+    .addColumn('type', sql`room_types_enum`, col => col.notNull())
+    .addColumn('place_id', 'uuid', col => col.references('places.id').onDelete('restrict').notNull())
+    .addColumn('position', 'integer')
+    .addColumn('created_at', 'timestamp', col => col.notNull())
+    .addColumn('updated_at', 'timestamp', col => col.notNull())
+    .addColumn('deleted_at', 'timestamp')
+    .execute()
+
+  await db.schema
+    .createIndex('rooms_type')
+    .on('rooms')
+    .column('type')
+    .execute()
+
+  await db.schema
+    .createIndex('rooms_place_id')
+    .on('rooms')
+    .column('place_id')
+    .execute()
+
+  await db.schema
+    .createIndex('rooms_deleted_at')
+    .on('rooms')
+    .column('deleted_at')
+    .execute()
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('rooms').execute()
+
+  await db.schema.dropType('room_types_enum').execute()
+}
