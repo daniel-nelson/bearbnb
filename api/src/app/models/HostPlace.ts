4740c6d22f57b586a923d700c7301b2a17856137
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2026-06-30 11:04:37 -0500

    Generate HostPlace model
    ```console
    pnpm psy g:model --help
    pnpm psy g:model --no-serializer HostPlace Host:belongs_to Place:belongs_to
    ```

---
+++ api/src/app/models/HostPlace.ts
@@ -0,0 +1,27 @@
+import { Decorators, SoftDelete } from '@rvoh/dream'
+import { DreamColumn } from '@rvoh/dream/types'
+import ApplicationModel from '@models/ApplicationModel.js'
+import Host from '@models/Host.js'
+import Place from '@models/Place.js'
+
+const deco = new Decorators<typeof HostPlace>()
+
+@SoftDelete()
+export default class HostPlace extends ApplicationModel {
+  public override get table() {
+    return 'host_places' as const
+  }
+
+  public id: DreamColumn<HostPlace, 'id'>
+  public createdAt: DreamColumn<HostPlace, 'createdAt'>
+  public updatedAt: DreamColumn<HostPlace, 'updatedAt'>
+  public deletedAt: DreamColumn<HostPlace, 'deletedAt'>
+
+  @deco.BelongsTo('Host', { on: 'hostId' })
+  public host: Host
+  public hostId: DreamColumn<HostPlace, 'hostId'>
+
+  @deco.BelongsTo('Place', { on: 'placeId' })
+  public place: Place
+  public placeId: DreamColumn<HostPlace, 'placeId'>
+}
